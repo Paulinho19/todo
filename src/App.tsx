e1b176986cdf0f9fc2ebd2c68b476bbbfa640840
Paulo <paulohsantosrocha@gmail.com>
2025-03-24 13:42:11 -0300

    chore: start setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="w-full h-full justify-center items-center flex flex-col space-y-2 bg-neutral-800 text-neutral-100"></div>
+  );
+};
+
+export default App;
